4e13933483c9ec7438271dcbf9526e783e88c167
Alejandro Rojas Jara <alerojas126@hotmail.com>
2017-04-12 22:44:18 -0600

    Codigo funcional
    Recibe los inputs del form, crea el objeto y se lo manda al Facade, este
    procesa los datos, si son validos realiza el "insert", si no devuelve
    los mensajes de error.

--- Facade/Facade.php
+++ Facade/Facade.php
@@ -1,116 +1,296 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <style>
+        .error {color: #FF0000;}
+    </style>
+</head>
+<body>
 
 <?php
 
-class Book {
-    private $author;
-    private $title;
-    function __construct($title_in, $author_in) {
-        $this->author = $author_in;
-        $this->title  = $title_in;
+class Person {
+    private $identification;
+    private $fullName;
+    private $birth_place;
+    private $birth_day;
+    private $gender;
+    #private $masDatos; ...
+
+    function __construct($id_in, $name_in, $birthday_in, $birthplace_in, $gender_in) { #,$masDatos_in) {
+        $this->identification = $id_in;
+        $this->fullName  = $name_in;
+        $this->birth_place  = $birthplace_in;
+        $this->birth_day  = $birthday_in;
+        $this->gender = $gender_in;
+        #$this->masDatos = $masDatos_in; ....
     }
-    function getAuthor() {
-        return $this->author;
+
+    function getID() {
+        return $this->identification;
     }
-    function getTitle() {
-        return $this->title;
+    function setID($newID){
+        $this->identification = $newID;
     }
-    function getAuthorAndTitle() {
-        return $this->getTitle().' by '.$this->getAuthor();
+
+    function getName() {
+        return $this->fullName;
     }
+    function setName($newName){
+        $this->fullName = $newName;
+    }
+
+    function getDayOfBirth() {
+        return $this->birth_day;
+    }
+    function setDayOfBirth($newDOB){
+        $this->birth_day = $newDOB;
+    }
+
+    function getPlaceOfBirth() {
+        return $this->birth_place;
+    }
+    function setPlaceOfBirth($newPOB){
+        $this->birth_place = $newPOB;
+    }
+
+    function getGender() {
+        return $this->gender;
+    }
+    function setGender($newGender){
+        $this->gender = $newGender;
+    }
+
+    #function getMasDatos()...
+    #function setMasDatos($newDato)...
 }
 
-class CaseReverseFacade {
-    public static function reverseStringCase($stringIn) {
-        $arrayFromString = ArrayStringFunctions::stringToArray($stringIn);
-        $reversedCaseArray = ArrayCaseReverse::reverseCase($arrayFromString);
-        $reversedCaseString = ArrayStringFunctions::arrayToString($reversedCaseArray);
-        return $reversedCaseString;
-    }
-}
-
-class ArrayCaseReverse {
-    private static $uppercase_array =
-        array('A', 'B', 'C', 'D', 'E', 'F',
-            'G', 'H', 'I', 'J', 'K', 'L',
-            'M', 'N', 'O', 'P', 'Q', 'R',
-            'S', 'T', 'U', 'V', 'W', 'X',
-            'Y', 'Z');
-    private static $lowercase_array =
-        array('a', 'b', 'c', 'd', 'e', 'f',
-            'g', 'h', 'i', 'j', 'k', 'l',
-            'm', 'n', 'o', 'p', 'q', 'r',
-            's', 't', 'u', 'v', 'w', 'x',
-            'y', 'z');
-    public static function reverseCase($arrayIn) {
-        $array_out = array();
-        for ($x = 0; $x < count($arrayIn); $x++) {
-            if (in_array($arrayIn[$x], self::$uppercase_array)) {
-                $key = array_search($arrayIn[$x], self::$uppercase_array);
-                $array_out[$x] = self::$lowercase_array[$key];
-            } elseif (in_array($arrayIn[$x], self::$lowercase_array)) {
-                $key = array_search($arrayIn[$x], self::$lowercase_array);
-                $array_out[$x] = self::$uppercase_array[$key];
-            } else {
-                $array_out[$x] = $arrayIn[$x];
+class Facade {
+    public static function process($person_data, $actionType) {
+        $dataValidation = new DataValidation();
+        $validData = $dataValidation->dataTypeValidation($person_data);
+        if($dataValidation->isValidData() == true){
+            #Opciones de CRUD
+            if($actionType == 0){
+                CRUD::create($person_data);
+                return null;
+            }
+            if($actionType == 1){
+                CRUD::read($person_data);
+                return null;
+            }
+            if($actionType == 2){
+                CRUD::update($person_data);
+                return null;
+            }
+            if($actionType == 3){
+                CRUD::delete($person_data);
+                return null;
             }
         }
-        return $array_out;
+        return $validData;
     }
 }
+#Clase para validaciones necesarias antes de llamar a CRUD
+class DataValidation {
+    private $validData;
 
-class ArrayStringFunctions {
-    public static function arrayToString($arrayIn) {
-        $string_out = NULL;
-        foreach ($arrayIn as $oneChar) {
-            $string_out .= $oneChar;
+    #Variable para determinar si los datos son validos o no
+    function __construct()
+    {
+        $this->validData = true;
+    }
+
+    public function dataTypeValidation($person_data) {
+        #------------------------------------------
+        # Verificacion de campo de ID o Cedula
+        #------------------------------------------
+        $person_id = $person_data->getID();
+        $person_id = self::clean_input($person_id);
+        $regex = '/^[0-9]{9}$/';
+        # No puede ser vacio
+        if(empty($person_id)){
+            $this->validData = false;
+            $person_data ->setID("El campo Cedula no puede estar vacio!");
         }
-        return $string_out;
+        # Formato especifico
+        elseif (!preg_match($regex, $person_id)) {
+            $this->validData = false;
+            $person_data->setID("Formato de Cedula incorrecto. Deben ser 9 numeros. Ejemplo: 123456789");
+        }
+        else{ $person_data ->setID("");}
+
+        #----------------------------------------
+        # Verificacion de campo de Nombre
+        #----------------------------------------
+        $person_name = $person_data ->getName();
+        $person_name = self::clean_input($person_name);
+        # No puede ser vacio
+        if(empty($person_name)){
+            $this->validData = false;
+            $person_data ->setName("El campo Nombre no puede estar vacio!");
+        }
+        # Formato especifico
+        elseif (!preg_match("/^[a-zA-Z ]*$/",$person_name)) {
+            $this->validData = false;
+            $person_data->setName("Solo se permiten letras y espacios!");
+        }else{ $person_data ->setName("");}
+
+        #----------------------------------------
+        # Verificacion de fecha de Naciimiento
+        #----------------------------------------
+        $person_DOB = $person_data ->getDayOfBirth();
+        $person_DOB = self::clean_input($person_DOB);
+        # No puede ser vacio
+        if(empty($person_DOB)){
+            $this->validData = false;
+            $person_data ->setDayOfBirth("La Fecha de Nacimiento no puede estar vacia!");
+        }
+        # Formato especifico
+        elseif (!DateTime::createFromFormat('d/m/Y', $person_DOB)) {
+            $this->validData = false;
+            $person_data ->setDayOfBirth("Formato de fecha incorrecto. Deberia ser DD/MM/AAAA");
+        }else{ $person_data ->setDayOfBirth("");}
+
+        #-------------------------------------------
+        # Verificacion de lugar de Nacimiento
+        #------------------------------------------
+        $person_POB = $person_data->getPlaceOfBirth();
+        # No puede ser vacio
+        if(empty($person_POB)){
+            $this->validData = false;
+            $person_data ->setPlaceOfBirth("El campo Lugar de nacimiento no puede estar vacio!");
+        }
+        else{ $person_data ->setPlaceOfBirth("");}
+
+        #------------------------------------------
+        # Verificacion de genero
+        #------------------------------------------
+        $person_gender = $person_data->getGender();
+        # No puede ser vacio
+        if(empty($person_gender)){
+            $this->validData = false;
+            $person_data ->setGender("Por favor selecciono el genero.");
+        }
+        else{$person_data ->setGender("");}
+
+        #------------------------------------------
+        # Otras verificaciones...
+        #------------------------------------------
+
+        # Devolvemos mensajes de error si hay alguno.
+        return $person_data;
     }
-    public static function stringToArray($stringIn) {
-        return str_split($stringIn);
+
+    static function clean_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    function isValidData(){
+        return $this->validData;
     }
 }
-
-
-writeln('BEGIN TESTING FACADE PATTERN');
-echo nl2br("\n");
-
-$book = new Book('Design Patterns', 'Gamma, Helm, Johnson, and Vlissides');
-
-writeln('Original book title: '.$book->getTitle());
-echo nl2br("\n");
-
-$bookTitleReversed = CaseReverseFacade::reverseStringCase($book->getTitle());
-
-writeln('Reversed book title: '.$bookTitleReversed);
-echo nl2br("\n");
-
-writeln('END TESTING FACADE PATTERN');
-echo nl2br("\n");
+#Clase encargada de funciones CRUD
+class CRUD {
+    public static function create($person_data) {
+        writeln("CREATE :: Insertando datos en la Base de Datos");
+        return $person_data;
+    }
+    public static function read($person_data) {
+        writeln("Buscando datos de la persona en la Base de Datos");
+        return $person_data;
+    }
+    public static function update($person_data) {
+        writeln("Actualizando datos de la persona en la Base de Datos");
+        return $person_data;
+    }
+    public static function delete($person_data) {
+        writeln("Eliminando datos de la persona en la Base de Datos");
+        return $person_data;
+    }
+}
 
 function writeln($line_in) {
-    echo $line_in."&lt;br/&gt;";
+    echo $line_in."<br/>\n";
 }
 
+# variable de datos y mensajes de error
+$idErr   = $nameErr   = $birthDayErr   = $birthPlaceErr   = $genderErr   = ""; # = $masDatosErr
+$idInput = $nameInput = $birthdayInput = $birthPlaceInput = $genderInput = ""; # = $masDatosInput
+
+#ACCION DE SUBMIT
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "<h2>Prueba de Facade:</h2>";
+
+    writeln('COMENZANDO PRUEBA DE PATRON FACHADA');
+    writeln('');
+    #Creamos objecto de Person con la informacion ingresada.
+    $idInput = $_POST["idInput"];
+    $nameInput = $_POST["nameInput"];
+    $birthdayInput = $_POST["birthdayInput"];
+    $birthPlaceInput = $_POST["birthPlaceInput"];
+    if (empty($_POST["genderInput"])) {
+        $genderInput = null;
+    } else {
+        $genderInput = $_POST["genderInput"];
+    }
+    # $masDatosInput = $_POST["masDatosInput"];
+
+
+    $person = new Person($idInput,$nameInput,$birthdayInput, $birthPlaceInput, $genderInput); # ,$masDatosInput);
+
+    writeln('Solicitando registro....');
+
+
+    $register_result = Facade::process($person, 0);
+    if(empty($register_result)){
+           writeln('Insercion realizada correctamente!');
+    }
+    else{
+        $idErr = $register_result->getID();
+        $nameErr = $register_result->getName();
+        $birthDayErr = $register_result->getDayOfBirth();
+        $genderErr = $register_result->getGender();
+        # $masDatosErr = $register_result->getMasDatos();
+        writeln('Hay datos invalidos! Por favor revise los campos.');
+    }
+
+    writeln('');
+    writeln('FIN DE PRUEBA DE PATRON FACHADA');
+}
 ?>
 
-<html>
-<body>
+<h2>Datos personales:</h2>
+<p><span class="error">* campo obligatorio.</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    Cedula: <input type="text" name="idInput" value="<?php echo $idInput;?>">
+    <span class="error">* <?php echo $idErr;?></span>
+    <br><br>
+    
+    Nombre Completo: <input type="text" name="nameInput" value="<?php echo $nameInput;?>">
+    <span class="error">* <?php echo $nameErr;?></span>
+    <br><br>
+        
+    Fecha De Nacimiento: <input type="text" name="birthdayInput" value="<?php echo $birthdayInput;?>">
+    <span class="error">* <?php echo $birthDayErr;?></span>
+    <br><br>
 
-<form action="Bienvenido.php" method="post">
-    Nombre: <input type="text" name="nombre"><br>
-    FechaDeNacimiento: <input type="text" name="fecha de nacimiento"><br>
-    Cedula: <input type="text" name="cedula"><br>
-    CedulaPadre: <input type="text" name="cedula padre"><br>
-    CedulaMadre: <input type="text" name="cedula madre"><br>
-    Sexo: <input type="text" name="sexo"><br>
-    Nacionalidad: <input type="text" name="nacionalidad"><br>
-    Edad: <input type="text" name="edad"><br>
-    Hijos: <input type="text" name="hijos"><br>
-    Matrimonios: <input type="text" name="matrimonios"><br>
-    LugarDeVotacion: <input type="text" name="lugar de votacion"><br>
-    <input type="submit">
+    Lugar De Nacimiento: <textarea name="birthPlaceInput" rows="5" cols="40"><?php echo $birthPlaceInput;?></textarea>
+    <br><br>
+
+    <!-- Mas Datos: <input type="text" name="masDatosInput" value=" < ?php echo $birthdayInput;?>"></textarea> -->
+    <!-- <span class="error">* < ?php echo $masDatosErr;?></span> -->
+    <!-- <br><br> -->
+        
+    Genero:
+    <input type="radio" name="genderInput" <?php if (isset($genderInput) && $genderInput=="Femenino") echo "checked";?> value="Femenino">Femenino
+    <input type="radio" name="genderInput" <?php if (isset($genderInput) && $genderInput=="Masculino") echo "checked";?> value="Masculino">Masculino
+    <span class="error">* <?php echo $genderErr;?></span>
+    <br><br>
+    <input type="submit" name="submit" value="Submit">
 </form>
-
 </body>
 </html>
